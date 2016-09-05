7ff7653b2b9f13d856a34840d5b8ffe44eb460b5
Patrick Snape <patricksnape@gmail.com>
2016-09-05 11:53:35 +0100

    Fix Manifest error

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,17 @@
+REM need to set manifest for VS2008
+REM otherwise exes crash (not sure why)
+set MANIFEST=no
+if "%VS_MAJOR%" == "9" (
+    set MANIFEST=yes
+)
 
 cd win32
 cscript configure.js prefix=%LIBRARY_PREFIX% include=%LIBRARY_INC% ^
-        lib=%LIBRARY_LIB% sodir=%LIBRARY_BIN% iconv=yes zlib=yes
+        lib=%LIBRARY_LIB% sodir=%LIBRARY_BIN% iconv=yes zlib=yes vcmanifest=%MANIFEST%
 if errorlevel 1 exit 1
 
-nmake
+nmake /f Makefile.msvc
 if errorlevel 1 exit 1
 
-nmake install
+nmake /f Makefile.msvc install
 if errorlevel 1 exit 1
